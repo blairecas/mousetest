5dc5a0f0f69c8c97338816a5d630fe6bd8d0afde
blairecas <chaminealex@mail.ru>
2024-03-10 02:30:59 +0300

    bug in PP memory release in ppt3

---
+++ 9_comp_tstr0.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo.
+echo ===========================================================================
+echo Compiling 
+echo ===========================================================================
+..\..\php5\php.exe -c ..\..\php5\ -f ..\scripts\preprocess.php tstr0.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+..\..\macro11\macro11.exe -ysl 32 -yus -m ..\..\macro11\sysmac.sml -l _tstr0.lst _tstr0.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+
+echo.
+echo ===========================================================================
+echo Linking
+echo ===========================================================================
+..\..\php5\php.exe -c ..\..\php5\ -f ..\scripts\lst2bin.php _tstr0.lst ./release/tstr0.sav sav
+..\..\macro11\rt11dsk.exe d moutst.dsk .\release\tstr0.sav >NUL
+..\..\macro11\rt11dsk.exe a moutst.dsk .\release\tstr0.sav >NUL
+
+echo.
